b167733b4e5d7a07516cb3b3c06a5dc52b86bb7e
angel-torres <alas4224@gmail.com>
2020-05-10 00:51:43 -0500

    added more content to the sidebar

--- client/src/app/layouts/default/default.module.ts
+++ client/src/app/layouts/default/default.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { DefaultComponent } from './default.component';
 import { DashboardComponent } from 'src/app/modules/dashboard/dashboard.component';
 import { RouterModule } from '@angular/router';
-import { MatSidenavModule } from '@angular/material';
+import { MatSidenavModule, MatDividerModule } from '@angular/material';
 import { PostsComponent } from 'src/app/modules/posts/posts.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 
@@ -17,7 +17,9 @@ import { SharedModule } from 'src/app/shared/shared.module';
     CommonModule,
     RouterModule,
     SharedModule,
-    MatSidenavModule 
+    MatSidenavModule,
+    MatDividerModule
+
   ]
 })
 export class DefaultModule { }
--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { MatDividerModule, MatIconModule, MatToolbarModule, MatButtonModule, MatMenuModule } from '@angular/material';
+import { MatDividerModule, MatIconModule, MatToolbarModule, MatButtonModule, MatMenuModule, MatListModule } from '@angular/material';
 // import { FlexLayoutModule } from "@angular/flex-layout";
 
 @NgModule({
@@ -18,7 +18,8 @@ import { MatDividerModule, MatIconModule, MatToolbarModule, MatButtonModule, Mat
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
-    MatMenuModule
+    MatMenuModule,
+    MatListModule
     // FlexLayoutModule
   ],
   exports: [
