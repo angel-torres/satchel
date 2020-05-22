b67b584f9c8fbf00f8e23a0142b51fcd550dd1bc
angel-torres <alas4224@gmail.com>
2020-05-21 23:04:13 -0500

    added contacts component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
+import { ContactsComponent } from './modules/contacts/contacts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    ContactsComponent,
   ],
   imports: [
     BrowserModule,
