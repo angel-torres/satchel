d6d1820c420758e68f60d2949ee6f438ff0a1a28
angel-torres <alas4224@gmail.com>
2020-05-21 23:07:34 -0500

    added todos component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DefaultModule } from './layouts/default/default.module';
 import { ContactsComponent } from './modules/contacts/contacts.component';
+import { TodosComponent } from './modules/todos/todos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactsComponent,
+    TodosComponent,
   ],
   imports: [
     BrowserModule,
