9223697a125e1d788023a28c35214e46e6297503
angel-torres <alas4224@gmail.com>
2020-05-23 18:32:06 -0500

    added interface for News

--- client/src/app/shared/components/news-card/news-card.component.ts
+++ client/src/app/shared/components/news-card/news-card.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ReaderModeService } from 'src/app/modules/reader-mode.service';
 
+interface News {
+  title: string;
+  date: string;
+  imageUrl: string;
+  body: string;
+}
+
 @Component({
   selector: 'app-news-card',
   templateUrl: './news-card.component.html',
   styleUrls: ['./news-card.component.scss']
 })
 export class NewsCardComponent implements OnInit {
-  @Input('news') news: any;
+  @Input('news') news: News;
   @Input('readerMode') readerMode: string;
 
   constructor( private readerModeService: ReaderModeService) { }
