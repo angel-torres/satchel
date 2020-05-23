5e33b2a0ec9f2409bcb443ddd8a02bee3f5ea5ec
angel-torres <alas4224@gmail.com>
2020-05-23 18:33:40 -0500

    added interface for announcement

--- client/src/app/shared/components/announcement-card/announcement-card.component.ts
+++ client/src/app/shared/components/announcement-card/announcement-card.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface Announcement {
+  title: string;
+  date: string;
+  body: string;
+}
+
 @Component({
   selector: 'app-announcement-card',
   templateUrl: './announcement-card.component.html',
   styleUrls: ['./announcement-card.component.scss']
 })
 export class AnnouncementCardComponent implements OnInit {
-  @Input('announcement') announcement: any;
+  @Input('announcement') announcement: Announcement;
   @Input('readerMode') readerMode: string;
 
   constructor() { }
