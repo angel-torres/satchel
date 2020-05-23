6b6618bd07f6604c51b9150985a8897717b9ef5d
angel-torres <alas4224@gmail.com>
2020-05-22 20:36:11 -0500

    added interface for contact

--- client/src/app/shared/components/contact-card/contact-card.component.ts
+++ client/src/app/shared/components/contact-card/contact-card.component.ts
@@ -1,16 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
 
+interface Contact {
+  contactImg: string;
+  email: string;
+  name: string;
+  phone: string;
+}
+
 @Component({
   selector: 'app-contact-card',
   templateUrl: './contact-card.component.html',
   styleUrls: ['./contact-card.component.scss']
 })
 export class ContactCardComponent implements OnInit {
-  @Input('contact') contact = {}
+  @Input('contact') contact: Contact
 
   constructor() { }
 
-  ngOnInit() {
-  }
+  ngOnInit() { }
 
 }
