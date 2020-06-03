fea7d3b447c305ea8e7f42988265365913ea623c
angel-torres <alas4224@gmail.com>
2020-06-03 00:10:34 -0500

    was able to add a signup handler; still working on implementing the logic

--- client/src/app/modules/auth/signup/signup.component.ts
+++ client/src/app/modules/auth/signup/signup.component.ts
@@ -1,3 +1,4 @@
+import { AuthService } from './../auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  handleSignup(event) {
+    event.preventDefault();
+    this.authService.login()
+    console.log("signing in!!");
   }
 
 }
