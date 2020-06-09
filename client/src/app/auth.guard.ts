f688c48e9f2b9c02b2796beccdd76118272f9dd7
angel-torres <alas4224@gmail.com>
2020-06-08 23:12:50 -0500

    removed dist folder from .gitignore

--- client/src/app/auth.guard.ts
+++ client/src/app/auth.guard.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, CanActivate } from '@angular/router';
 import { Observable } from 'rxjs';
+import { AuthService } from './modules/auth/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
+  constructor(private auth: AuthService){
+
+  }
+
   canActivate(
     next: ActivatedRouteSnapshot, state: RouterStateSnapshot
     ): Observable<boolean> | Promise<boolean> | boolean {
-      return true;
+      return false;
   }
 }
